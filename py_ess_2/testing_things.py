d20b39bc4a945ff2063f126c41b9c082955abbe9
joegrigg64 <joegrigg64@gmail.com>
2022-02-01 15:51:34 -0500

    progress

--- py_ess_2/testing_things.py
+++ py_ess_2/testing_things.py
@@ -1,4 +1,5 @@
 import math
+from platform import platform, machine, processor, system, version, python_implementation, python_version_tuple
 
 print('testing the things')
 
@@ -7,5 +8,15 @@ math_dir = dir(math)
 # for d in math_dir:
 #     print(d)
 
-print(math.ceil(45.000000001))
-print(math.floor(46.99999999999))
+# print(math.ceil(45.000000001))
+# print(math.floor(46.99999999999))
+
+print(platform())
+print(platform(1))
+print(platform(0, 1))
+print(machine())
+print(processor()) # name of CPU
+print(system())
+print(version())
+print(python_implementation())
+print(python_version_tuple())
